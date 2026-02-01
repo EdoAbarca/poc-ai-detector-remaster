50b945b05213be93d6024016ad552ad93489b919
EdoAbarca <eduardo.abarca.c@usach.cl>
2026-01-31 22:11:38 -0300

    [Issue 5, US-001D] fix: Docker Python and linting errors
    - Update Dockerfile to use Ubuntu 22.04 (has Python 3.11 in repos)
    - Fix JavaScript Standard Style errors in postcss.config.js:
      * Remove trailing commas
      * Remove extra semicolons
    - Fix JavaScript Standard Style errors in utils.js:
      * Add spaces before function parentheses
    
    Resolves Docker build failure and super-linter errors.

--- react-frontend/src/utils.js
+++ react-frontend/src/utils.js
@@ -1,14 +1,14 @@
 /**
  * Simple utility function for testing purposes
  */
-export function add(a, b) {
+export function add (a, b) {
   return a + b
 }
 
-export function multiply(a, b) {
+export function multiply (a, b) {
   return a * b
 }
 
-export function greet(name) {
+export function greet (name) {
   return `Hello, ${name}!`
 }
